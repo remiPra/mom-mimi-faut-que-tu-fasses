f8ae592463ca927e452ed2207286bd29155288cc
remiPra <remipradere@gmail.com>
2022-06-18 14:46:02 +0200

    try pwa

--- next.config.js
+++ next.config.js
@@ -4,3 +4,12 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const widthPWA = require("next-pwa");
+module.exports = widthPWA({
+  pwa: {
+    dest:"public",
+    register:true,
+    skipWaiting:true,
+  },
+})
