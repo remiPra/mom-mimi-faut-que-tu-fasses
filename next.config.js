3a4b6b3dbce35e55c1d0bf1dae0b3797d481aa90
remiPra <remipradere@gmail.com>
2022-06-21 12:05:34 +0200

    push with server.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,28 @@
-
-
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
-
-
-
-module.exports = withPWA({
+module.exports = {
 	reactStrictMode: true,
-	pwa: {
-		dest: "public",
-		register: true,
-		skipWaiting: true,
-		runtimeCaching,
-		disable: process.env.NODE_ENV === "development",
-	
+	webpack5: false,
+	async headers() {
+	  return [
+		{
+		  // matching all API routes
+		  source: "/api/:path*",
+		  headers: [
+			{ key: "Access-Control-Allow-Credentials", value: "true" },
+			{ key: "Access-Control-Allow-Origin", value: "*" },
+			{ key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+			{ key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+		  ]
+		}
+	  ]
 	}
-});
+  }
+  
+  const widthPWA = require("next-pwa");
+  module.exports = widthPWA({
+	pwa: {
+	  dest:"public",
+	  register:true,
+	  skipWaiting:true,
+	},
+  })
+  
