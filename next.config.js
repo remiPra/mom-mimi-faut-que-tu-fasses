e0abdf55b9c8c4f1bc2eb7623dd4938c9693bc7c
remiPra <remipradere@gmail.com>
2022-06-21 08:54:22 +0200

    essai 21.06.2022

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ module.exports = nextConfig
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
+module.exports = {
+	// Webpack 5 is enabled by default
+	// You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
+	webpack5: true,
+  }
+
 module.exports = withPWA({
 	reactStrictMode: true,
 	pwa: {
