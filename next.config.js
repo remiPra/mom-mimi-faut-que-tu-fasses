902d186ee88b915e3628d2796417cc30ba6fce56
remiPra <remipradere@gmail.com>
2022-06-21 11:03:42 +0200

    service-worker.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
 
 const withPWA = require("next-pwa");
 const runtimeCaching = require("next-pwa/cache");
 
-module.exports = {
-	// Webpack 5 is enabled by default
-	// You can still use webpack 4 while upgrading to the latest version of Next.js by adding the "webpack5: false" flag
-	webpack5: true,
-  }
+
 
 module.exports = withPWA({
 	reactStrictMode: true,
@@ -21,6 +12,7 @@ module.exports = withPWA({
 		register: true,
 		skipWaiting: true,
 		runtimeCaching,
-		buildExcludes: [/middleware-manifest.json$/]
+		disable: process.env.NODE_ENV === "development",
+		sw:"service-worker.js"
 	}
 });
